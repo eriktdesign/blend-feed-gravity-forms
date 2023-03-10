76b59cb427786b65de7608b371ba60ea9adf0405
Erik Teichmann <erik@inverseparadox.net>
2023-03-10 16:18:10 -0500

    Adds some field mappings

--- inc/class-blend-gfeed.php
+++ inc/class-blend-gfeed.php
@@ -245,47 +245,74 @@ class BlendGFeed extends GFFeedAddOn {
 						'tooltip' => esc_html__('This is the tooltip', 'blend-gfeed'),
 						'class'   => 'small',
 					),
-					array(
-						'label'   => esc_html__('Textbox', 'blend-gfeed'),
-						'type'    => 'text',
-						'name'    => 'mytextbox',
-						'tooltip' => esc_html__('This is the tooltip', 'blend-gfeed'),
-						'class'   => 'small',
-					),
-					array(
-						'label'   => esc_html__('My checkbox', 'blend-gfeed'),
-						'type'    => 'checkbox',
-						'name'    => 'mycheckbox',
-						'tooltip' => esc_html__('This is the tooltip', 'blend-gfeed'),
-						'choices' => array(
-							array(
-								'label' => esc_html__('Enabled', 'blend-gfeed'),
-								'name'  => 'mycheckbox',
-							),
-						),
-					),
+					// array(
+					// 	'label'   => esc_html__('Textbox', 'blend-gfeed'),
+					// 	'type'    => 'text',
+					// 	'name'    => 'mytextbox',
+					// 	'tooltip' => esc_html__('This is the tooltip', 'blend-gfeed'),
+					// 	'class'   => 'small',
+					// ),
+					// array(
+					// 	'label'   => esc_html__('My checkbox', 'blend-gfeed'),
+					// 	'type'    => 'checkbox',
+					// 	'name'    => 'mycheckbox',
+					// 	'tooltip' => esc_html__('This is the tooltip', 'blend-gfeed'),
+					// 	'choices' => array(
+					// 		array(
+					// 			'label' => esc_html__('Enabled', 'blend-gfeed'),
+					// 			'name'  => 'mycheckbox',
+					// 		),
+					// 	),
+					// ),
 					array(
 						'name'      => 'mappedFields',
 						'label'     => esc_html__('Map Fields', 'blend-gfeed'),
 						'type'      => 'field_map',
 						'field_map' => array(
 							array(
-								'name'       => 'email',
+								'name'       => 'party.email',
 								'label'      => esc_html__('Email', 'blend-gfeed'),
-								'required'   => 0,
+								'required'   => 1,
 								'field_type' => array('email', 'hidden'),
 								'tooltip' => esc_html__('This is the tooltip', 'blend-gfeed'),
 							),
 							array(
-								'name'     => 'name',
-								'label'    => esc_html__('Name', 'blend-gfeed'),
+								'name'     => 'party.firstName',
+								'label'    => esc_html__('First Name', 'blend-gfeed'),
+								'required' => 1,
+							),
+							array(
+								'name'     => 'party.lastName',
+								'label'    => esc_html__('Last Name', 'blend-gfeed'),
+								'required' => 1,
+							),
+							array(
+								'name'       => 'party.homePhone',
+								'label'      => esc_html__('Home Phone', 'blend-gfeed'),
+								'required'   => 0,
+								'field_type' => 'phone',
+							),
+							array(
+								'name'     => 'loanPurposeType',
+								'tooltip'  => esc_html__( 'Should be one of the following: CONSTRUCTION, PURCHASE, REFINANCE' ),
+								'label'    => esc_html__('Loan Purpose', 'blend-gfeed'),
+								'required' => 0,
+							),		
+							array(
+								'name'     => 'property.address.state',
+								'label'    => esc_html__('Property Address State', 'blend-gfeed'),
 								'required' => 0,
 							),
 							array(
-								'name'       => 'phone',
-								'label'      => esc_html__('Phone', 'blend-gfeed'),
-								'required'   => 0,
-								'field_type' => 'phone',
+								'name'     => 'property.address.zipCode',
+								'label'    => esc_html__('Property Address ZIP code', 'blend-gfeed'),
+								'required' => 0,
+							),							
+							array(
+								'name'     => 'property.type',
+								'tooltip'  => esc_html__( 'Should be one of the following: SINGLE_FAMILY, CONDOMINIUM, TOWNHOUSE, TWO_TO_FOUR_UNIT_PROPERTY, COOPERATIVE, MANUFACTURED_OR_MOBILE_HOME' ),
+								'label'    => esc_html__('Property Type', 'blend-gfeed'),
+								'required' => 0,
 							),
 						),
 					),
@@ -338,5 +365,52 @@ class BlendGFeed extends GFFeedAddOn {
 		$key = rgar($settings, 'apiKey');
 
 		return true;
+	}
+
+	/**
+	 * Outputs the menu icon
+	 *
+	 * @return string SVG tag
+	 */
+	public function get_menu_icon() {
+		return '<svg width="22" height="20" version="1.0" xmlns="http://www.w3.org/2000/svg"
+		width="1687.000000pt" height="2790.000000pt" viewBox="0 0 1687.000000 2790.000000"
+		preserveAspectRatio="xMidYMid meet">
+	   
+	   <g transform="translate(0.000000,2790.000000) scale(0.100000,-0.100000)"
+	   fill="#000000" stroke="none">
+	   <path d="M0 26200 l0 -1700 5073 -3 c4569 -3 5084 -5 5197 -19 775 -99 1381
+	   -360 1915 -824 1055 -917 1520 -2470 1135 -3788 -221 -756 -700 -1426 -1445
+	   -2023 -55 -44 -311 -239 -570 -433 -258 -194 -487 -366 -508 -383 l-38 -29
+	   103 -76 c141 -104 590 -431 1523 -1111 435 -316 835 -608 890 -648 229 -167
+	   297 -214 304 -211 19 8 447 324 589 435 238 188 405 336 643 573 542 540 944
+	   1088 1285 1755 414 808 643 1610 731 2555 24 257 24 870 0 1130 -76 822 -252
+	   1529 -568 2275 -76 178 -276 583 -370 745 -607 1058 -1423 1892 -2457 2512
+	   -221 132 -700 368 -937 461 -585 230 -1128 371 -1730 447 -458 58 -29 54
+	   -5632 57 l-5133 3 0 -1700z"/>
+	   <path d="M1470 21166 c-773 -108 -1370 -722 -1460 -1501 -6 -61 -10 -1936 -10
+	   -5725 0 -4803 2 -5651 15 -5741 89 -669 527 -1203 1154 -1409 410 -135 856
+	   -108 1246 74 126 59 196 102 335 206 107 80 406 298 1325 965 198 144 594 432
+	   880 640 286 208 608 442 715 520 107 78 404 294 660 480 256 186 515 374 575
+	   418 61 44 157 115 215 157 58 42 222 162 365 265 373 270 425 309 425 315 0 3
+	   -31 26 -68 52 -37 26 -125 90 -197 142 -71 52 -170 125 -220 160 -206 150
+	   -266 193 -600 437 -586 428 -1272 927 -1423 1037 -79 56 -195 141 -258 188
+	   l-114 84 -587 -426 c-324 -235 -689 -501 -813 -591 l-225 -164 -3 1101 c-1
+	   606 -1 1594 0 2195 l3 1094 530 -386 c292 -212 753 -548 1025 -746 491 -358
+	   633 -461 1178 -857 607 -440 930 -675 1252 -910 184 -134 382 -278 440 -320
+	   58 -42 323 -235 590 -430 628 -457 774 -563 868 -629 42 -30 248 -178 457
+	   -331 209 -152 602 -438 874 -636 271 -197 555 -404 630 -459 75 -55 290 -212
+	   478 -348 451 -329 596 -452 811 -687 439 -481 730 -1063 842 -1685 43 -243 53
+	   -362 54 -645 0 -294 -12 -438 -59 -696 -190 -1029 -794 -1929 -1650 -2462
+	   -340 -211 -798 -383 -1210 -452 -343 -58 75 -53 -5447 -57 l-5068 -3 0 -1700
+	   0 -1700 5123 3 c3864 2 5151 6 5242 15 621 62 1009 131 1462 263 1118 325
+	   2100 901 2913 1708 292 290 517 552 742 866 485 675 841 1413 1063 2202 255
+	   904 338 1705 269 2571 -150 1889 -1068 3606 -2583 4831 -140 113 -1611 1189
+	   -2073 1516 -84 60 -224 161 -1023 744 -401 292 -791 576 -865 630 -74 54 -317
+	   230 -540 392 -223 162 -497 362 -610 444 -113 82 -783 569 -1490 1084 -707
+	   514 -1629 1185 -2050 1491 -421 306 -1147 834 -1615 1175 -887 646 -1064 774
+	   -1255 912 -206 148 -478 266 -715 309 -120 22 -401 27 -525 10z"/>
+	   </g>
+	   </svg>';
 	}
 }
